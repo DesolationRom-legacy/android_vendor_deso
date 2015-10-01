482642a6e934b796094a18112f10e5fe041cbc35
Jacob McSwain <jacob.a.mcswain@gmail.com>
2015-10-01 17:13:44 +0200

    PROPOSED: Change versioning for better ota updates in the future
    Desolation-1.10.1-shamu-Nightly if I were to build right now
    
    Change-Id: I04526caccac2cab187e59815244c04738a0e7ab4
    Signed-off-by: Jacob McSwain <jacob.a.mcswain@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -4,32 +4,33 @@ TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst deso_,,$(TARGET_PRODUCT_SHORT))
 
 ROM_VERSION_MAJOR := 1
-ROM_VERSION_MINOR := $(shell date -u +%m)
+ROM_VERSION_MINOR := $(shell date -u +%m.$d)
+ROM_VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)
 
 ifdef BUILDTYPE_NIGHTLY
 	ROM_BUILDTYPE := Nightly
 	changelog_date := $(shell date -d "1 day ago" '+%m-%d-%Y')
 endif
+
 ifdef BUILDTYPE_WEEKLY
 	ROM_BUILDTYPE := Weekly
 	changelog_date := $(shell date -d "1 week ago" '+%m-%d-%Y')
 endif
+
 ifdef BUILDTYPE_EXPERIMENTAL
 	ROM_BUILDTYPE := Experimental
 	changelog_date := $(shell date -d "2 weeks ago" '+%m-%d-%Y')
 endif
 
-ROM_VERSION := $(shell date -u +%m.%d).$(BUILD_ID)
-
 ifdef BUILDTYPE_RELEASE
 	ROM_BUILDTYPE := Release
-	ROM_VERSION := $(BUILD_ID)-v$(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)
 	changelog_date := $(shell date -d "1 month ago" '+%m-%d-%Y')
 endif
 
 ifndef ROM_BUILDTYPE
 	ROM_BUILDTYPE := Unofficial
 endif
+
 ifndef changelog_date
 	 changelog_date := $(shell date -d "2 weeks ago" '+%m-%d-%Y')
 endif
@@ -39,5 +40,6 @@ DESO_VERSION := $(ROM_BUILDTYPE)
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.modversion=Desolation-$(TARGET_PRODUCT_SHORT) \
+	ro.delta.device=$(DESO_PRODUCT) \
 	ro.deso.version=$(ROM_VERSION) \
 	rom.buildtype=$(ROM_BUILDTYPE)
